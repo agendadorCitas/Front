c28696c5375aad551a477686e5be6433635ec64f
Sebastian Vasquez Tamayo <svasquez531@misena.edu.co>
2023-06-30 00:54:29 -0500

    correccion

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+import _interopRequireDefault from "@babel/runtime/helpers/interopRequireDefault";
 var _app = _interopRequireDefault(require("./app.js"));
 _app["default"].listen(_app["default"].get("port"), function () {
   console.log("Se ha conectado al puerto: ".concat(_app["default"].get("port"), "\n    ").concat(process.env.API, ", ").concat(_app["default"].get("port"), "\n    "));
