14b48a53cf6b9c6f9e87719e8aa20da2726f444e
Sebastian Vasquez Tamayo <svasquez531@misena.edu.co>
2023-06-30 01:46:09 -0500

    version completada

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app.js"));
-_app["default"].listen(_app["default"].get("port"), function () {
-  console.log("Se ha conectado al puerto: ".concat(_app["default"].get("port"), "\n    ").concat(process.env.API, ", ").concat(_app["default"].get("port"), "\n    "));
-});
